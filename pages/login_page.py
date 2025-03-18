03db0bff93cb6d51e847b8ac2957caf46a85a8c2
Michael <michael-ya92@yandex.ru>
2025-03-18 13:39:49 +0300

    add login page

---
+++ pages/login_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        url = self.browser.current_url
+        assert LoginPageLocators.LOGIN_URL in url, "Incorrect URL"
+        # assert True
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM) == True, "No login form"
+        # assert True
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM) == True, "No registration form"
+        # assert True
--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,11 @@ from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
 
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -9,8 +14,4 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
  
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+
